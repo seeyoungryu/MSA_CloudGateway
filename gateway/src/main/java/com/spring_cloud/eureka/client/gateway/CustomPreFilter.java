6ab4ca2a7248b74673670e502eeb2c4320e0d696
seeyoungryu <seayeux@gmail.com>
2024-08-04 19:11:57 +0900

    add - gateway class files

---
+++ gateway/src/main/java/com/spring_cloud/eureka/client/gateway/CustomPreFilter.java
@@ -0,0 +1,29 @@
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
+
+@Component
+public class CustomPreFilter implements GlobalFilter, Ordered {
+
+    private static final Logger logger = Logger.getLogger(CustomPreFilter.class.getName());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest response = exchange.getRequest();
+        logger.info("Pre Filter: Request URI is " + response.getURI());
+        // Add any custom logic here
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
