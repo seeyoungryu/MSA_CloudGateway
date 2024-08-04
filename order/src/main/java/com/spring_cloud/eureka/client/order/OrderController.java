e114ae04763f8450392361f0730aef39f4f624cc
seeyoungryu <seayeux@gmail.com>
2024-08-04 17:36:39 +0900

    update - Order,Product - 컨트롤러 로직 변경 - yml 포트 변경 (19092,19093~4)

--- order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -1,21 +1,18 @@
 package com.spring_cloud.eureka.client.order;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/order")
 public class OrderController {
 
-    private final OrderService orderService;
-
-    @GetMapping("/order/{orderId}")
-    public String getOrder(@PathVariable String orderId) {
-        return orderService.getOrder(orderId);
+    @GetMapping
+    public String getOrder() {
+        return "Order details";
     }
-
 }
+
+
